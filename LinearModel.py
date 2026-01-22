5d68fcee88e1802c458e0c3dbecfc81064c76f4a
Avraaj <avr99j@gmail.com>
2026-01-22 10:17:40 -0800

    Added Cyprus plot data

--- LinearModel.py
+++ LinearModel.py
@@ -9,11 +9,12 @@ X = lifesat[["GDP per capita (USD)"]].values
 y = lifesat[["Life satisfaction"]].values
 
 lifesat.plot(kind='scatter', grid=True, x="GDP per capita (USD)", y = "Life satisfaction")
-plt.axis([223_500, 62_500, 4, 9])
+plt.axis([23_500, 62_500, 4, 9])
 plt.show()
 
 model = LinearRegression()
 
 model.fit(X, y)
 
-
+X_new = [[37_655.2]]
+print(model.predict(X_new))
